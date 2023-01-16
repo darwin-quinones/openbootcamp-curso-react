1e3c1e14ab16b96066f0c41d4cea0516985f9196
darwin-quinones <darwinquinones735@gmail.com>
2023-01-16 09:42:59 -0500

    Se termina el clase de componentes y jerarquia

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import Greeting from './components/pure/greeting.jsx';
+import Greeting from './components/pure/greeting';
+import GreetingF from './components/pure/greetingF';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {/* componente greeting */}
-        <Greeting></Greeting>
+        {/* <Greeting name='Darwin'></Greeting> */}
+        <GreetingF name='Darwinn'></GreetingF>
       </header>
     </div>
   );
--- src/components/pure/greeting.jsx
+++ src/components/pure/greeting.jsx
@@ -1,3 +1,5 @@
+// CLASE COMPONENTES Y JERARQUIA
+
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
@@ -6,18 +8,44 @@ class Greeting extends Component {
 
     // props datos(caracteristicas de los elementos)
     // estado = informacion privada que se le pasa aun componente
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            age: 21
+        }
+    }
+
+
     render() {
         return (
             <div>
-             <h1>Hellooo</h1>
-                
+             <h1>Hellooo {this.props.name}!</h1>
+                <h2>Your age is: {this.state.age}</h2>
+
+                <div>
+                    <button onClick={this.birthday}>
+                        Cumplir años
+                    </button>
+                </div>
             </div>
         );
     }
+
+    // array function
+    birthday = () =>{
+        this.setState((prevState) =>(
+            {
+                age: prevState.age + 1
+            }
+        ))
+    } 
 }
 
-// Los propTypes = contenido que se le puede pasar a un componente des de otro componente de orden superior
+// Los propTypes = contenido que se le puede pasar a un componente desde de otro componente de orden superior
 Greeting.propTypes = {
+    // definir el tipo de los propstypes  
+    name: PropTypes.string
 
 };
 
