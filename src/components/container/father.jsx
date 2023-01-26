f7a5c9194ed38a7c594ceaf6bd1e10d337d93e87
darwin-quinones <darwinquinones735@gmail.com>
2023-01-26 15:59:34 -0500

    Se termina la clase de eventos

--- src/components/container/father.jsx
+++ src/components/container/father.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Child from '../pure/child';
 
 const Father = () => {
+
+    const [name, setName] = useState('Darwin');
+
+    /** 
+     * Example of a @function that is call from
+     * the child component 
+     * 
+     */
+
+    function showMessage(text){
+        alert(`Message received: ${text}`)
+    }
+
+    /**
+     * Update name
+    */
+   function updateName(newName){
+        setName(newName)
+   }
+
     return (
-        <div>
-            <Child></Child>
+        <div style={{ background:'tomato', padding:'10px' }}>
+            <Child name={ name } send={ showMessage } update={ updateName }></Child>
         </div>
     );
 }
--- src/components/pure/child.jsx
+++ src/components/pure/child.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
-const Child = ({name}) => {
+// useRef -> reference HTML elements-referenciar elementos
+const Child = ({name, send, update}) => {
+
+    // ossociate element so we can get the value anytime
+    const messageRef = useRef('')
+    const nameRef = useRef('')
+
 
     /**
      * @functions 
      */
     function pressButton(){
-        alert('Default Text')
+        // get input value
+        const text = messageRef.current.value;
+        alert(`Input text: ${text} `)
     }
 
     function pressButtonParants(text){
         alert(`Text: ${text} `)
     }
 
+
+    function submitName(e){
+        e.preventDefault()
+        update(nameRef.current.value)
+    }
+
     return (
-        <div>
+        <div style={{background:'Cyan', padding:'30px' }}>
             <p onMouseOver={ () => console.log('On Mouse Over') }>child component</p>
+            <p>Hola, {name}</p>
             <button onClick={ () => console.log('pressed button 1') }>
                 Button 1
             </button>
@@ -27,9 +42,26 @@ const Child = ({name}) => {
                 Button 3
             </button>
             <input 
-                placeholder='Insert a text'
+                placeholder='Insert a text to your father'
                 onFocus={() => console.log('Input Focused')}
+                onChange={(e) => console.log('Input Changed: ', e.target.value)}
+                onCopy={() => console.log('Copied text from imput')}
+                ref={messageRef}
              />
+            
+            {/* function 'send' comes from father component 
+             and it is call here with a parameter */}
+             <button onClick={ () => send(messageRef.current.value) }>
+                Send Message
+             </button>
+
+             {/* form */}
+             <div style={{ marginTop: '20px' }}>
+                <form onSubmit={ submitName }>
+                    <input ref={ nameRef } placeholder='New Name' />
+                    <button type='submit'>Update Name</button>
+                </form>
+             </div>
         </div>
     );
 }
