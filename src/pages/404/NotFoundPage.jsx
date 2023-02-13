cce193a1ea40d8865e3ef2a273e43007f72825b6
Darwin Quiñones <darwinquinones735@gmail.com>
2023-02-12 20:57:02 -0500

    Se hacen las buenas practicas de estructuración de projectos

---
+++ src/pages/404/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Notfounpage = () =>{
+    return(
+        <div>
+            <h1>404 - Page not found :( </h1>
+        </div>
+    )
+}
+
+export default Notfounpage
